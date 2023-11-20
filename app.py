3fb218b6709f102128d4f0f5f3701c162cff255e
Nehemiah60 <nehemiabosire41@gmail.com>
2023-11-20 18:03:51 +0300

    profile pic upload

--- app.py
+++ app.py
@@ -57,7 +57,13 @@ def logout():
 
 #Function to save a picture image in our db
 
-
+def save_profpic(form_picture):
+    random_hex = secrets.token_hex(8)
+    f_name, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/images', picture_fn)
+    form_picture.save(picture_path)
+    return picture_fn
     
 #user profile
 @app.route('/profile', methods=["GET", "POST"])
